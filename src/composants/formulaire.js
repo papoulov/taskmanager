2c9260d49e64acb03fd9ef601dfca51585663a02
papoulov <papoulov@gmail.com>
2023-06-03 05:14:28 -0400

    another commit

--- src/composants/formulaire.js
+++ src/composants/formulaire.js
@@ -2,55 +2,58 @@ import { useState } from "react";
 import { myColors } from "../assets/colors";
 import { useDate } from "../utils/date";
 
-function Formulaire(props){
-    
-    const [maTache, setMaTache]= useState('');
-    
-const todayDate = useDate();
+function Formulaire(props) {
+
+    const [maTache, setMaTache] = useState('');
+
+    const todayDate = useDate();
 
 
-    const handleChange = (e)=>{
+    const handleChange = (e) => {
         setMaTache(e.target.value)
     };
 
     //arrow fonction ES6
-    const handleSubmit = (e) =>{
-        
+    const handleSubmit = (e) => {
+
         e.preventDefault();
 
-        if(maTache !==''){
+        if (maTache !== '') {
             props.Ajout({
-                id: Math.floor(Math.random()*10000),
+                id: Math.floor(Math.random() * 10000),
                 text: maTache,
-                color: myColors[Math.floor(Math.random()*10)],
+                color: myColors[Math.floor(Math.random() * 10)],
             });
 
             setMaTache('')
         }
     };
 
-    return(<div style={{backgroundColor:'#eee', width:'100%', display:'flex', flexDirection:'column', alignItems:'center', textAlign:'center'}}>
-         <div style={{width:'100%', textAlign:'right', marginRight:20, marginTop:5, fontSize:12}}>
-        {todayDate.date} {todayDate.time}
+    return (<div style={{
+        backgroundColor: 'rgb(221, 221, 221, 0.5)', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center',
+        textAlign: 'center', marginTop: 10
+    }}>
+        <div style={{ width: '100%', textAlign: 'right', marginRight: 20, marginTop: 5, fontSize: 12 }}>
+            {todayDate.date} {todayDate.time}
         </div>
-        <div style={{marginBottom:5}}>
+        <div style={{ marginBottom: 5 }}>
             <h1>
-                Bonjour Papou<br/> {props.taches.length ===0 && `Quel est votre programme aujourd'hui ?`} </h1>
-           
-            {props.taches.length ===0 ? 'Vous n\'avez aucune tache active. Vous pouvez commencer par ajouter des taches': `Vous avez ${props.taches.length} tache actives`}
+                Bonjour Papou<br /> {props.taches.length === 0 && `Quel est votre programme aujourd'hui ?`} </h1>
+
+            {props.taches.length === 0 ? 'Vous n\'avez aucune tache active. Vous pouvez commencer par ajouter des taches' : `Vous avez ${props.taches.length} tache actives`}
         </div>
-       
-    <form className="form" onSubmit={handleSubmit}>
-        <input type="text"
-        className="input" 
-        placeholder="Enter votre tache" value={maTache} 
-        onChange={handleChange}/>
-        <button>
-            AJOUTER
-        </button>
+
+        <form className="form" onSubmit={handleSubmit}>
+            <input type="text"
+                className="input"
+                placeholder="Enter votre tache" value={maTache}
+                onChange={handleChange} />
+            <button>
+                AJOUTER
+            </button>
         </form>
-        </div>
-    ) 
+    </div>
+    )
 }
 
 export default Formulaire;
